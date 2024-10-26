64566576d7e174340382f60e957c87aaff34d76c
Lance <ljpruitt123@gmail.com>
2024-10-25 19:09:00 -0700

    fix: admin cors added token header to fetchList function

--- admin/src/pages/List.jsx
+++ admin/src/pages/List.jsx
@@ -10,9 +10,9 @@ const List = ({ token }) => {
 
   const fetchList = async () => {
     try {
-      const response = await axios.get("https://ecommerce-fs-backend.vercel.app/api/product/list");
-
-
+      const response = await axios.get(`${backendUrl}/api/product/list`, {
+        headers: { token },
+      });
       if (response.data.success) {
         setList(response.data.products);
       } else {
